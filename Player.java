e027862cfbf797351c6cecd883140b241a8b9bc5
EvelynPryadkin <evelynpryadkin@gmail.com>
2025-02-19 10:16:43 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,35 +2,24 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Player {
-    private Circle shape;
+    private Circle player;
     private double x, y;
 
     public Player(double startX, double startY) {
-        this.x = startX;
-        this.y = startY;
-        shape = new Circle(x, y, 15, Color.YELLOW);
+        x = startX;
+        y = startY;
+        player = new Circle(x, y, 15, Color.YELLOW);
     }
 
-    public void moveTo(double newX, double newY) {
-        this.x = newX;
-        this.y = newY;
-        shape.setCenterX(x);
-        shape.setCenterY(y);
-    }
+    public Circle getShape() { return player; }
 
-    public boolean intersects(Treasure treasure) {
-        return shape.getBoundsInParent().intersects(treasure.getShape().getBoundsInParent());
-    }
+    public double getX() { return x; }
+    public double getY() { return y; }
 
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public Circle getShape() {
-        return shape;
+    public void move(double dx, double dy) {
+        x += dx;
+        y += dy;
+        player.setCenterX(x);
+        player.setCenterY(y);
     }
 }
