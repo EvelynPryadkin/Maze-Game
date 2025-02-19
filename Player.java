0b6bbfbd07ee1fbbd7597ddd0de72c9b7eca3a62
EvelynPryadkin <evelynpryadkin@gmail.com>
2025-02-19 11:26:49 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,24 +2,28 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Player {
-    private Circle player;
-    private double x, y;
+    private Circle circle;
 
-    public Player(double startX, double startY) {
-        x = startX;
-        y = startY;
-        player = new Circle(x, y, 15, Color.YELLOW);
+    public Player(double x, double y) {
+        circle = new Circle();
+        circle.setCenterX(x);
+        circle.setCenterY(y);
+        circle.setRadius(15);
+        circle.setFill(Color.YELLOW); // Player is a yellow circle
     }
 
-    public Circle getShape() { return player; }
-
-    public double getX() { return x; }
-    public double getY() { return y; }
+    public Circle getShape() { return circle; }
 
     public void move(double dx, double dy) {
-        x += dx;
-        y += dy;
-        player.setCenterX(x);
-        player.setCenterY(y);
+        circle.setCenterX(circle.getCenterX() + dx);
+        circle.setCenterY(circle.getCenterY() + dy);
+    }
+
+    public double getX() { return circle.getCenterX(); }
+    public double getY() { return circle.getCenterY(); }
+
+    public void setPosition(double x, double y) {
+        circle.setCenterX(x);
+        circle.setCenterY(y);
     }
 }
