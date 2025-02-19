6004ddda000468e062a6485e3750763e8fbba8b4
EvelynPryadkin <evelynpryadkin@gmail.com>
2025-02-19 11:25:41 -0600

    Update Treasure.java

--- Treasure.java
+++ Treasure.java
@@ -7,21 +7,37 @@ public class Treasure {
     private Rectangle rect;
     private Color color;
 
+    private double snap(double x) {
+        return Math.floor(x / 40) * 40.0;
+    }
+
     public Treasure(double x, double y, Color c) {
-        color = c;
-        rect = new Rectangle(x, y, 30, 30);
+        this.color = c;
+        rect = new Rectangle();
+        rect.setX(x - 15.0);
+        rect.setY(y - 15.0);
+        rect.setWidth(30.0);
+        rect.setHeight(30.0);
         rect.setFill(color);
     }
 
     public Shape getShape() { return rect; }
 
-    public void write(PrintWriter out) {
-        String colorStr = (color == Color.RED) ? "red" : (color == Color.GREEN) ? "green" : "blue";
-        out.println((int) rect.getX() + " " + (int) rect.getY() + " " + colorStr);
+    public void setLocation(double x, double y) {
+        rect.setX(x - 15.0);
+        rect.setY(y - 15.0);
     }
 
-    public boolean isCollected(double x, double y) {
-        return rect.contains(x, y);
+    public void snapToGrid(double x, double y) {
+        rect.setX(snap(x) + 5.0);
+        rect.setY(snap(y) + 5.0);
+    }
+
+    public void write(PrintWriter out) {
+        String colorStr = "red";
+        if (color == Color.BLUE) colorStr = "blue";
+        else if (color == Color.GREEN) colorStr = "green";
+
+        out.println(Math.round(rect.getX()) + " " + Math.round(rect.getY()) + " " + colorStr);
     }
 }
-
